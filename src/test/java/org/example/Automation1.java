11c61a3690901017f9d085098205183df53c1fcf
DivyaKorat <divyakorat1985@gmail.com>
2020-06-30 17:39:52 +0100

    MavenHW

---
+++ src/test/java/org/example/Automation1.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Automation1 {
+    static WebDriver driver;
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","C:\\soft\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.tesco.com/");
+
+    }
+}
